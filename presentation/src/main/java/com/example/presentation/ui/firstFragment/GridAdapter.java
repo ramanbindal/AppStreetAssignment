8a2ef57e655cf39bb0293833cec5f56a7c98a3eb
ramanbindal <raman.bindal@cubastion.com>
2019-05-01 18:10:38 +0530

    minor changes

--- presentation/src/main/java/com/example/presentation/ui/firstFragment/GridAdapter.java
+++ presentation/src/main/java/com/example/presentation/ui/firstFragment/GridAdapter.java
@@ -1,18 +1,3 @@
-/*
- * Copyright 2018 Google LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
 package com.example.presentation.ui.firstFragment;
 
@@ -191,8 +176,7 @@ public class GridAdapter extends RecyclerView.Adapter<GridAdapter.ImageViewHolde
         void setImage(final int adapterPosition, Bitmap decodedByte) {
             // Load the imageView with Glide to prevent OOM error when the imageView drawables are very large.
 
-            requestManager
-                    .load(decodedByte)
+            requestManager.load(decodedByte)
                     .listener(new RequestListener<Drawable>() {
                         @Override
                         public boolean onLoadFailed(@Nullable GlideException e, Object model,
